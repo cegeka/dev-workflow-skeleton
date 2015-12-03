f3a12ea33b54f7d328e4d8092104f301982b824c
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-03 16:54:13 +0100

    Gulp: build app and build vendor

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,19 @@
-var gulp = require('gulp');
+var gulp = require("gulp");
+var mainBowerFiles = require("main-bower-files");
 
-gulp.task('default', function() {
+gulp.task("default", ["build:app", "build:vendor"]);
+
+gulp.task("build:app", function() {
     return gulp
         .src("src/**/*")
         .pipe(gulp.dest("dist"));
+});
+
+
+gulp.task("build:vendor", function() {
+    return gulp
+        .src(mainBowerFiles({
+            checkExistence: true
+        }))
+        .pipe(gulp.dest("dist/vendor"));
 });
