4560cfa7aabbe01d4b6f486523048d558331d63e
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-04 09:16:17 +0100

    Add clean before gulp build

--- README.md
+++ README.md
@@ -15,5 +15,5 @@ The goal of this innovation center is to setup a small project that can be used 
 2. `npm install` in project root
 3. `bower install` in project root
 4. `gulp` in project root
-5. `http-server` in project root
-6. Go to http://localhost:8080/dist
+5. `http-server` in dist
+6. Go to http://localhost:8080
--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require("gulp");
+var runSequence = require("run-sequence");
+var del = require("del");
 var mainBowerFiles = require("main-bower-files");
 
-gulp.task("default", ["build:app", "build:vendor"]);
+gulp.task("default", function(callback) {
+    runSequence("clean", "build", callback);
+});
+
+gulp.task("clean", function(){
+    return del("dist/")
+});
+
+gulp.task("build", ["build:app", "build:vendor"]);
 
 gulp.task("build:app", function() {
     return gulp
         .src("src/**/*")
         .pipe(gulp.dest("dist"));
 });
-
 
 gulp.task("build:vendor", function() {
     return gulp
