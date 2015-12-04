fa17cfb51c6c69976eea8a17ed8f8426ef8ebae3
Gilliam <gi11i4m@gmail.com>
2015-12-04 09:44:17 +0000

    Transpile ES2015 (no modules) (closes #15)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,28 +1,48 @@
 import gulp from "gulp";
 import runSequence from "run-sequence";
 import del from "del";
+import babel from "gulp-babel";
 import mainBowerFiles from "main-bower-files";
 
-gulp.task("default", (callback) => {
-    runSequence("clean", "build", callback);
-});
+gulp.task("default", callback =>
+    runSequence("clean", "build", callback)
+);
 
-gulp.task("clean", () => {
-    return del("dist/");
-});
+gulp.task("clean", () =>
+    del("dist/")
+);
 
-gulp.task("build", ["build:app", "build:vendor"]);
+gulp.task("build", ["build:app", "build:assets", "build:vendor"]);
 
-gulp.task("build:app", () => {
-    return gulp
-        .src("src/**/*")
-        .pipe(gulp.dest("dist"));
-});
+gulp.task("build:app", ["build:app:js", "build:app:html"]);
 
-gulp.task("build:vendor", () => {
-    return gulp
-        .src(mainBowerFiles({
-            checkExistence: true
-        }))
-        .pipe(gulp.dest("dist/vendor"));
-});
+gulp.task("build:app:js", () =>
+    gulp
+    .src("src/**/*.js")
+    .pipe(babel({
+        presets: ["es2015"]
+    }))
+    .pipe(gulp.dest("dist"))
+);
+
+gulp.task("build:app:html", () =>
+    gulp
+    .src("src/**/*.html")
+    .pipe(gulp.dest("dist"))
+);
+
+gulp.task("build:assets", () =>
+    gulp
+    .src("src/assets/**/*", {
+        base: "src"
+    })
+    .pipe(gulp.dest("dist"))
+);
+
+gulp.task("build:vendor", () =>
+    gulp
+    .src(mainBowerFiles({
+        checkExistence: true
+    }))
+    .pipe(gulp.dest("dist/vendor"))
+);
