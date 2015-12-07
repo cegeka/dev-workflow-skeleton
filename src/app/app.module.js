8acabd249e4402ef55e606f54090d1a5f59cff9f
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-07 14:06:11 +0100

    Enable html5Mode

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -1,10 +1,11 @@
 angular
-.module("petcupidApp", [
-    /* Shared modules */
-    "app.core",
-    "app.widgets",
+    .module("petcupidApp", [
+        /* Shared modules */
+        "app.core",
+        "app.widgets",
 
-    /* Components */
-    "app.gallery",
-    "app.setup-date"
-]);
+        /* Components */
+        "app.gallery",
+        "app.setup-date"
+    ])
+    .config($locationProvider => $locationProvider.html5Mode(true));
