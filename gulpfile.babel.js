aa38a8a1af3cc69774f2afbc5ed97c624c2c7374
Gilliam <gi11i4m@gmail.com>
2015-12-07 10:52:04 +0000

    Gulp watch and serve (closes #35)

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,14 +8,34 @@ import eslint from "gulp-eslint";
 import imagemin from "gulp-imagemin";
 import pngquant from "imagemin-pngquant";
 import mainBowerFiles from "main-bower-files";
+import browserSync from "browser-sync";
+
+const server = browserSync.create("dev-workflow-skeleton");
+
+const dirs = {
+    src: "src",
+    dest: "dist"
+};
+
+const paths = {
+    vendor: `${dirs.dest}/vendor`,
+    html: "src/**/*.html",
+    js: "src/**/*.js",
+    css: "src/assets/css/*",
+    img: "src/assets/img/*",
+    ngNewRouter: `node_modules/angular-new-router/${dirs.dest}/router.es5.js`,
+    bower: "bower.json"
+};
 
 gulp.task("default", callback =>
     runSequence("clean", "build", callback)
 );
 
 gulp.task("clean", () =>
-    del("dist/")
+    del(`${dirs.dest}/`)
 );
+
+gulp.task("dev", ["build", "watch", "serve"]);
 
 gulp.task("build", ["build:app", "build:assets", "build:vendor", "lint"]);
 
@@ -25,41 +45,49 @@ gulp.task("build:assets", ["build:assets:css", "build:assets:img"]);
 
 gulp.task("build:vendor", ["build:vendor:npm", "build:vendor:bower"]);
 
+gulp.task("watch", () => {
+    gulp.watch(paths.html, ["build:app:html"]);
+    gulp.watch(paths.js, ["build:app:js"]);
+    gulp.watch(paths.css, ["build:assets:css"]);
+    gulp.watch(paths.img, ["build:assets:img"]);
+    gulp.watch(paths.bower, ["build:vendor:bower"]);
+});
+
 gulp.task("build:app:js", () =>
     gulp
-    .src("src/**/*.js")
+    .src(paths.js)
     .pipe(babel({
         presets: ["es2015"]
     }))
     .pipe(ngAnnotate())
     .pipe(uglify())
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(dirs.dest))
 );
 
 gulp.task("build:app:html", () =>
     gulp
-    .src("src/**/*.html")
-    .pipe(gulp.dest("dist"))
+    .src(paths.html)
+    .pipe(gulp.dest(dirs.dest))
 );
 
 gulp.task("build:assets:css", () =>
     gulp
-    .src("src/assets/css/*", {
-        base: "src"
+    .src(paths.css, {
+        base: dirs.src
     })
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(dirs.dest))
 );
 
 gulp.task("build:assets:img", () =>
     gulp
-    .src("src/assets/img/*", {
-        base: "src"
+    .src(paths.img, {
+        base: dirs.src
     })
     .pipe(imagemin({
         progressive: true,
         use: [pngquant()]
     }))
-    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest(dirs.dest))
 );
 
 gulp.task("build:vendor:bower", () =>
@@ -67,18 +95,30 @@ gulp.task("build:vendor:bower", () =>
     .src(mainBowerFiles({
         checkExistence: true
     }))
-    .pipe(gulp.dest("dist/vendor"))
+    .pipe(gulp.dest(paths.vendor))
 );
 
 gulp.task("build:vendor:npm", () =>
     gulp
-    .src("node_modules/angular-new-router/dist/router.es5.js")
-    .pipe(gulp.dest("dist/vendor"))
+    .src(paths.ngNewRouter)
+    .pipe(gulp.dest(paths.vendor))
 );
 
-gulp.task("lint", () => {
-    return gulp.src(["gulpfile.babel.js", "src/app/**/*.js"])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
-});
+gulp.task("lint", () =>
+    gulp.src(["gulpfile.babel.js", paths.js])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError())
+);
+
+gulp.task("serve", () =>
+    server.init({
+        ui: {
+            port: 8081
+        },
+        port: 8080,
+        server: {
+            baseDir: dirs.dest
+        }
+    })
+);
