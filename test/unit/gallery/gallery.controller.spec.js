0dcdaec6b00886c9cb91b5191284371c3de46e99
BLannoo <brunolannoo@gmail.com>
2015-12-09 14:54:51 +0100

    implemented gallery.contorller test

--- test/unit/gallery/gallery.controller.spec.js
+++ test/unit/gallery/gallery.controller.spec.js
@@ -1,8 +1,14 @@
-describe('GalleryCtrl', () => {
-  describe('#selectPet()', () => {
+import GalleryCtrl from "gallery/gallery.controller";
 
+describe('GalleryCtrl', () => {
+  describe('#selectPet()', () => {  
     it('should select a pet', () => {
-      
+    	let ctrl = new GalleryCtrl();
+    	let event = {stopPropagation: function(){}};
+
+  		ctrl.selectPet(event, "MissPoes");
+
+  		ctrl.selectedPet.should.equal("MissPoes");
     });
   });
 });
