789564d7341f49841ee559f2df4f927b0fddccbf
BLannoo <brunolannoo@gmail.com>
2015-12-14 14:43:08 +0100

    added slow flag to protractor

--- test/e2e/protractor.conf.js
+++ test/e2e/protractor.conf.js
@@ -5,7 +5,8 @@ exports.config = {
     framework: "mocha",
     mochaOpts: {
         reporter: "spec",
-        timeout: 120000
+        timeout: 120000,
+        slow: 5000
     },
     seleniumServerJar: "../../node_modules/protractor/selenium/selenium-server-standalone-2.48.2.jar",
     baseUrl: "http://localhost:8080",
