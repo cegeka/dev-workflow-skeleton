7a8b82d5386fe23a9280c18021e9c4f71cc505db
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-15 16:15:29 +0100

    Set produces on resource

--- dws-rest/src/main/java/be/cegeka/dws/rest/PetResource.java
+++ dws-rest/src/main/java/be/cegeka/dws/rest/PetResource.java
@@ -1,16 +1,24 @@
 package be.cegeka.dws.rest;
 
+import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 
 import org.springframework.stereotype.Component;
+
+import be.cegeka.dws.domain.Pet;
 
 @Component
 @Path("/pet")
 public class PetResource {
 
 	@GET
-	public String getPet() {
-		return "I'm a cool pet!.";
+	@Produces(APPLICATION_JSON)
+	public Pet getPet() {
+		Pet pet = new Pet();
+		pet.setName("Pet");
+		return pet;
 	}
 }
