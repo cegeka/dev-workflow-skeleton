d977844e6d8658515d7b82fea2aabe61442ff1bf
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-15 13:39:07 +0100

    Add application annotations

--- dws-rest/src/main/java/be/cegeka/dws/Application.java
+++ dws-rest/src/main/java/be/cegeka/dws/Application.java
@@ -1,7 +1,13 @@
 package be.cegeka.dws;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
 	public static void main(String[] args) {
