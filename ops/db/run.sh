c573a9db338c4a01803bd029c9822f4bd22966b2
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-24 13:40:08 +0100

    Run docker containers in custom bridge network (#71)

--- ops/db/run.sh
+++ ops/db/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f dws-db
-docker run -d -p 5432:5432 --name dws-db dws/db
+docker run -d -p 5432:5432 --net dws --name dws-db dws/db
--- ops/front-end/run.sh
+++ ops/front-end/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 docker rm -f dws-front-end
-docker run -d -p 80:80 --name dws-front-end dws/front-end
+docker run -d -p 80:80 --net dws --name dws-front-end dws/front-end
