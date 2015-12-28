0b310062ae732700bbe746bf8b2e7f0cbe1d8415
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-28 16:30:12 +0100

    Rename image for local db

--- ops/scripts/start-local-db.sh
+++ ops/scripts/start-local-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t dws_db_local ../dws/db/
+docker build -t dws_db ../dws/db/
 docker rm -f dws_db_local
-docker run -d -p 5432:5432 --name dws_db_local dws_db_local
+docker run -d -p 5432:5432 --name dws_db_local dws_db
