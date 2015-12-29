317124c73ac995a2826eac1fbf43877b5168330c
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-29 11:05:01 +0100

    Mount jenkins home folder

--- ops/scripts/start-jenkins.sh
+++ ops/scripts/start-jenkins.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+JENKINS_HOME="/var/opt/jenkins-dws"
+
+if [ ! -d $JENKINS_HOME ] 
+then
+    sudo mkdir -p $JENKINS_HOME
+    sudo chown 1000 $JENKINS_HOME
+fi
+
 docker build -t dws_jenkins ../jenkins/
 docker rm -f dws_jenkins
-docker run -d -p 8888:8080 --name dws_jenkins dws_jenkins
+docker run -p 8888:8080 -v $JENKINS_HOME:/var/jenkins_home  --name dws_jenkins dws_jenkins
