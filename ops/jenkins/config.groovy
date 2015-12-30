6fcd5db343e34017859422dee2bf8ab21b66a019
dietervanpeer <dieter.vanpeer@cegeka.be>
2015-12-30 08:51:37 +0100

    Clean up mvn install + use double quoted strings to enable variables to resolve

--- ops/jenkins/config.groovy
+++ ops/jenkins/config.groovy
@@ -8,8 +8,6 @@ jenkins.setLabelString("linux jdk8 mvn3");
 def jdk8 = new hudson.model.JDK("jdk8", "/usr/lib/jvm/java-8-oracle");
 jenkins.setJDKs([jdk8]);
 
-def mvnExtenstions = jenkins.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
-def mvnExtenstionsList = (mvnExtenstions.installations as List);
-mvnExtenstionsList.add(new hudson.tasks.Maven.MavenInstallation("mvn3", "/opt/apache-maven-3.3.9"));
-mvnExtenstions.installations = mvnExtenstionsList;
-mvnExtenstions.save();
+def mvn3 = new Maven.MavenInstallation("mvn3", "/opt/apache-maven-3.3.9");
+def mvnExtension = jenkins.getExtensionList(Maven.DescriptorImpl.class).get(0);
+mvnExtension.setInstallations(mvn3);
--- ops/scripts/start-jenkins.sh
+++ ops/scripts/start-jenkins.sh
@@ -3,7 +3,7 @@ JENKINS_HOME="/var/opt/jenkins-dws"
 
 if [ ! -d $JENKINS_HOME ] 
 then
-	echo "Creating mount point for JENKINS_HOME"
+	echo "Creating mount point for JENKINS_HOME, password please..."
     sudo mkdir -p $JENKINS_HOME
     sudo chown 1000 $JENKINS_HOME
 fi
