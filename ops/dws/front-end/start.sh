d2b5dbe01b5122d130d3187120444d71f302fb58
dietervanpeer <dieter.vanpeer@cegeka.be>
2016-01-04 22:02:11 +0100

    Don't add default port 8080 for apiProxy

--- ops/dws/front-end/start.sh
+++ ops/dws/front-end/start.sh
@@ -1,3 +1,3 @@
-(echo "upstream dws-back-end { server $dws_api_proxy:8080; } " && cat /etc/nginx/conf.d/dws.conf) > /tmp/dws.conf
+(echo "upstream dws-back-end { server $dws_api_proxy; } " && cat /etc/nginx/conf.d/dws.conf) > /tmp/dws.conf
 mv /tmp/dws.conf /etc/nginx/conf.d/dws.conf
 nginx -g "daemon off;"
