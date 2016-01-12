5e9dc23221f6cb381b6d29eb9df5505065c1d84f
dietervanpeer <dieter.vanpeer@cegeka.be>
2016-01-12 15:43:54 +0100

    Run mvn clean install in a container

---
+++ ops/maven/clean-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker network create -d bridge dws-maven
+
+docker build -t dws/db ../dws/db/
+docker rm -vf dws_db_maven
+docker run -d --net dws-maven --name dws_db_maven dws/db
+
+docker rm -vf dws_maven_clean_install
+docker run -it --net dws-maven --name dws_maven_clean_install -v $(pwd)/../../dws:/usr/src/dws -w /usr/src/dws maven:3.3-jdk-8 mvn clean install -Ddb.host=dws_db_maven -pl '!:dws-acc-tests'
+
+docker stop dws_maven_clean_install
+docker rm -v dws_maven_clean_install
+docker stop dws_db_maven
+docker rm -v dws_db_maven
+docker network rm dws-maven
