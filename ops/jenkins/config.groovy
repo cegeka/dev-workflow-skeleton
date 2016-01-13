a5bdb16d6e0cf36933fe4fe4d070258b647957b3
dietervanpeer <dieter.vanpeer@cegeka.be>
2016-01-13 16:02:09 +0100

    Run jenkins test stage in docker

--- ops/jenkins/config.groovy
+++ ops/jenkins/config.groovy
@@ -1,11 +1,5 @@
 import jenkins.model.*
-import hudson.tasks.*
-import org.jenkinsci.plugins.xvfb.*
 
 def jenkins = Jenkins.instance;
 jenkins.setNumExecutors(5);
-jenkins.setLabelString("linux docker xvfb");
-
-def xvfb = new XvfbInstallation("xvfb", "/usr/bin", []);
-def xvfbExtension = jenkins.getExtensionList(XvfbInstallation.DescriptorImpl.class).get(0);
-xvfbExtension.setInstallations(xvfb);
+jenkins.setLabelString("linux docker");
--- ops/scripts/build-maven.sh
+++ ops/scripts/build-maven.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
 docker build -t dws/mvn ../maven/base/
 docker build -t dws/mvn-front-end ../maven/front-end/
+docker build -t dws/mvn-e2e ../maven/e2e/
